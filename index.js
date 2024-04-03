d0f11fa935afb42e2072ff1ecaaa92a65c009bdb
Shanzeye <165150162+Shanzeye@users.noreply.github.com>
2024-04-03 23:41:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to number guessing game");
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed right number.");
+}
+else {
+    console.log("You guessed wrong number");
+}
+;
